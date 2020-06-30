7b9f3b27a1efa7ccdffbb2aff45f803429c93581
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-06-30 15:04:20 +1000

    finished demo

--- Window.py
+++ Window.py
@@ -3,6 +3,7 @@ from misc import *
 import ctypes
 import sys
 from Ray import Ray
+import time
 
 class Window():
     def __init__(self, w=320, h=240):
@@ -15,6 +16,8 @@ class Window():
         self.screen = pygame.display.set_mode((w, h))
         self.shapes = []
 
+        self.fpsLastTime = time.time()
+
     def frame(self):
 
         for i in self.shapes:
@@ -23,10 +26,10 @@ class Window():
         pygame.display.flip()
 
     def mouseEvent(self, x, y):
-        ray = Ray(self, x, y, -180)
-        pos = ray.cast()
-        print(pos, ray.x, ray.y)
-        self.drawLine(ray.x, ray.y, *pos, (0, 0, 255))
+        for i in range(0, 360, 5):
+            ray = Ray(self, x, y, i)
+            pos = ray.cast()
+            self.drawLine(ray.x, ray.y, *pos, (0, 0, 255))
 
     def drawEllipse(self, x, y, w, h, colour):
         pygame.draw.ellipse(self.screen, colour, (x, y, w, h))
@@ -51,6 +54,11 @@ class Window():
                     sys.exit()
             self.mouseEvent(*pygame.mouse.get_pos())
             self.frame()
+
+            timeNow = time.time()
+            frameTime = timeNow - self.fpsLastTime
+            pygame.display.set_caption(str(round(1/frameTime)))
+            self.fpsLastTime = timeNow
 
     def closest(self, x, y):
         closest = None
--- main.py
+++ main.py
@@ -6,5 +6,7 @@ from shapes import *
 win = Window(500, 500)
 Rect.Rect(win, 10, 10, 100, 100, (255, 0, 0))
 Ellipse.Ellipse(win, 150, 150, 100, 100, (0, 255, 0))
+Rect.Rect(win, 250, 50, 150, 60, (255, 255, 0))
+Rect.Rect(win, 35, 200, 75, 60, (0, 255, 255))
 
 win.loop()

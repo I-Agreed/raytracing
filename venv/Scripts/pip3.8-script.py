abc5f2f65f5c1d8abb4befb0007520d201897571
I_Agreed <48776053+I-Agreed@users.noreply.github.com>
2020-06-30 16:05:50 +1000

    redid venv
    had problems when i renamed the repo

--- venv/Scripts/pip3.8-script.py
+++ venv/Scripts/pip3.8-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\Brend\OneDrive\Desktop\github repos\raytracing-vision\venv\Scripts\python.exe"
+#!"C:\Users\Brend\OneDrive\Desktop\github repos\raytracing\venv\Scripts\python.exe"
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3.8'
 __requires__ = 'pip==19.0.3'
 import re
